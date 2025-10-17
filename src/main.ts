2bc74a306ca8e6d004c5bb806874b4b04297daea
Matías Gallardo <matiasgallardo196@gmail.com>
2025-10-17 22:44:12 +1100

    Update CORS configuration in main.ts to allow requests from the rental platform's production URL.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/^http:\/\/localhost:3000$/, /^http:\/\/127\.0\.0\.1:3000$/],
+    origin: [
+      /^http:\/\/localhost:3000$/,
+      /^http:\/\/127\.0\.0\.1:3000$/,
+      "https://rental-platform-sand.vercel.app",
+    ],
     credentials: true,
   });
   app.setGlobalPrefix("api");
